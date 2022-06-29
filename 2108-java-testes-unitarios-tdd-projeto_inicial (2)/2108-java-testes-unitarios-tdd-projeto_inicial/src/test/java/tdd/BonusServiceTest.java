6d873b3ab8f0ca7208446c9f15ebc00b23a3988a
Andrew Matos Ascar <89961173+AndrewAscar742@users.noreply.github.com>
2022-06-29 19:17:52 -0300

    Add files via upload

---
+++ 2108-java-testes-unitarios-tdd-projeto_inicial (2)/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/tdd/BonusServiceTest.java
@@ -0,0 +1,35 @@
+package tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+
+class BonusServiceTest {
+	static BonusService bonus = new BonusService();
+	
+	@Test
+	void bonusDeveRetornarZERO() {
+		BigDecimal big = bonus.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+		assertEquals(BigDecimal.ZERO, big);
+	}
+	
+	@Test
+	void bonusDeveRetornarPositivo() {
+		BigDecimal big = bonus.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+		assertEquals(new BigDecimal("250.00"), big);
+	}
+	
+	@Test
+	void bonusDeveRetornarSalarioExatamente10000() {
+		BigDecimal big = bonus.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+		assertEquals(new BigDecimal("1000.00"), big);
+	}
+
+}
